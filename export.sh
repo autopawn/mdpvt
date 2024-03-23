3efcbcc308af343848ac38a77b9ca83a45a3affe
Francisco Casas <franciscojacb@gmail.com>
2024-03-23 12:18:21 -0300

    Add script to export game to all platforms.

---
+++ export.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Change directory to script directory
+cd "$(dirname "$0")"
+
+pico8="$HOME/Videos/pico-8/pico8"
+
+rm -rf build
+mkdir build
+
+sed 's/--[^>].*//g' mdpvt.p8 \
+    | sed 's/ = /=/g' \
+    | sed 's/ == /==/g' \
+    | sed 's/ != /!=/g' \
+    | sed 's/ >= />=/g' \
+    | sed 's/ <= /<=/g' \
+    | sed 's/ > />/g' \
+    | sed 's/ < /</g' \
+    | sed 's/ [+] /+/g' \
+    | sed 's/ [*] /*/g' \
+    | sed 's/ - /-/g' \
+    | awk '!NF {if (++n <= 1) print; next}; {n=0;print}' \
+    > build/mdpvt_stripped.p8
+
+
+$pico8 build/mdpvt_stripped.p8 -export build/index.html
+zip build/index.zip build/index.html build/index.js
+
+$pico8 build/mdpvt_stripped.p8 -export build/mdvt.png
+
+$pico8 build/mdpvt_stripped.p8 -export build/mdvt.bin

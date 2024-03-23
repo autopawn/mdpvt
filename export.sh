c448753041c83a8b4576749ba211f29c4ae612c3
Francisco Casas <franciscojacb@gmail.com>
2024-03-23 12:42:54 -0300

    Properly export .p8.png cart.

--- export.sh
+++ export.sh
@@ -22,6 +22,7 @@ sed 's/--[^>].*//g' mdpvt.p8 \
     | sed 's/ [+] /+/g' \
     | sed 's/ [*] /*/g' \
     | sed 's/ - /-/g' \
+    | sed 's/^[/][/]/--/g' \
     | awk '!NF {if (++n <= 1) print; next}; {n=0;print}' \
     > build/mdpvt_stripped.p8
 
@@ -29,6 +30,6 @@ sed 's/--[^>].*//g' mdpvt.p8 \
 $pico8 build/mdpvt_stripped.p8 -export build/index.html
 zip build/index.zip build/index.html build/index.js
 
-$pico8 build/mdpvt_stripped.p8 -export "build/mdvt_$version.png"
+$pico8 build/mdpvt_stripped.p8 -export "build/mdvt_$version.p8.png"
 
 $pico8 build/mdpvt_stripped.p8 -export "build/mdvt_$version.bin"
